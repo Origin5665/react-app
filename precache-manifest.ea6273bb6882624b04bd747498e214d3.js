cd7fc836ce759a9d955dc86140fde0dd45183910
Aleksandr Bakumenko <santaxxxx@gmail.com>
2020-10-29 15:08:48 +0300

    Updates

---
+++ precache-manifest.ea6273bb6882624b04bd747498e214d3.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68738d6037e53cd004408b3ff6affdd8",
+    "url": "/Origin5665/react-social-network/index.html"
+  },
+  {
+    "revision": "251720dd23ba3f72a5a8",
+    "url": "/Origin5665/react-social-network/static/css/4.7238a937.chunk.css"
+  },
+  {
+    "revision": "7bfaeabde72fdef18bd1",
+    "url": "/Origin5665/react-social-network/static/css/5.2fd3b709.chunk.css"
+  },
+  {
+    "revision": "1d55ac70780f1baf7063",
+    "url": "/Origin5665/react-social-network/static/css/main.815e8e9b.chunk.css"
+  },
+  {
+    "revision": "e990cfeee448688cb4bf",
+    "url": "/Origin5665/react-social-network/static/js/2.15550bbd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Origin5665/react-social-network/static/js/2.15550bbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b2e063985e74925fe6",
+    "url": "/Origin5665/react-social-network/static/js/3.af43226b.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/Origin5665/react-social-network/static/js/3.af43226b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "251720dd23ba3f72a5a8",
+    "url": "/Origin5665/react-social-network/static/js/4.0c334385.chunk.js"
+  },
+  {
+    "revision": "7bfaeabde72fdef18bd1",
+    "url": "/Origin5665/react-social-network/static/js/5.9076c904.chunk.js"
+  },
+  {
+    "revision": "1d55ac70780f1baf7063",
+    "url": "/Origin5665/react-social-network/static/js/main.16efdfbe.chunk.js"
+  },
+  {
+    "revision": "d787ddec0b2aee828876",
+    "url": "/Origin5665/react-social-network/static/js/runtime-main.27b4b0a9.js"
+  },
+  {
+    "revision": "51c51123f70efd9287791a6a5b98e977",
+    "url": "/Origin5665/react-social-network/static/media/delete.51c51123.svg"
+  },
+  {
+    "revision": "2ca8291b3cc81513ea715da45d3bb100",
+    "url": "/Origin5665/react-social-network/static/media/done.2ca8291b.svg"
+  },
+  {
+    "revision": "e41f997a1661d3538a25b774622f5430",
+    "url": "/Origin5665/react-social-network/static/media/email.e41f997a.svg"
+  },
+  {
+    "revision": "dbe3845e39bfc35ac3391f34d2a3ec6f",
+    "url": "/Origin5665/react-social-network/static/media/girl.dbe3845e.svg"
+  },
+  {
+    "revision": "192a6620a687a9e7e51c0037973a1547",
+    "url": "/Origin5665/react-social-network/static/media/github-image.192a6620.svg"
+  },
+  {
+    "revision": "7746f5e4d82a2cf71a3d1f30fdef6ace",
+    "url": "/Origin5665/react-social-network/static/media/heart.7746f5e4.svg"
+  },
+  {
+    "revision": "2369d24d785f348feeb8b6cb8d555864",
+    "url": "/Origin5665/react-social-network/static/media/man.2369d24d.svg"
+  },
+  {
+    "revision": "b27c2af26f7be3982fd4defcc7000a67",
+    "url": "/Origin5665/react-social-network/static/media/newspaper.b27c2af2.svg"
+  },
+  {
+    "revision": "822077716b867ac9e76be1a7c3964ce1",
+    "url": "/Origin5665/react-social-network/static/media/settings.82207771.svg"
+  },
+  {
+    "revision": "9072b6a03727199a1429f520624715c7",
+    "url": "/Origin5665/react-social-network/static/media/song.9072b6a0.svg"
+  },
+  {
+    "revision": "12128e4a245497c002a00081a40c4a6c",
+    "url": "/Origin5665/react-social-network/static/media/spinner.12128e4a.svg"
+  },
+  {
+    "revision": "6c4e8b34a0c2f0ecb83a017d2552c2cf",
+    "url": "/Origin5665/react-social-network/static/media/telegram.6c4e8b34.svg"
+  },
+  {
+    "revision": "02723a8b181c646ad15095dd4786dac1",
+    "url": "/Origin5665/react-social-network/static/media/user-photo.02723a8b.png"
+  },
+  {
+    "revision": "5f6eb7caf50edf56c225361e8a8e629d",
+    "url": "/Origin5665/react-social-network/static/media/user.5f6eb7ca.svg"
+  }
+]);
